260aa2041c24e55dc22e257dcc509b473d288455
Louis Facun <louisfacun@gmail.com>
2023-06-16 13:12:03 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.numfig',
     #'sphinxcontrib.bibtex',
 ]
 #bibtex_bibfiles = ['refs.bib']
